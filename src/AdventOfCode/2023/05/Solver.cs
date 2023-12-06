bfa489c1ff0896f0f5e0be7feb97dc7ac3af688f
Eskil Høstad <eskil.hostad@huddly.com>
2023-12-06 07:40:12 +0100

    wip part 2 2023 day 5

--- src/AdventOfCode/2023/05/Solver.cs
+++ src/AdventOfCode/2023/05/Solver.cs
@@ -48,32 +48,74 @@ internal class Solver : ISolver
 
         var inputSections = input.Split("\n\n");
 
-        var seeds = inputSections[0].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)[1..].Select(long.Parse).ToArray();
+        var seedRanges = inputSections[0]
+            .Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)[1..]
+            .Select(long.Parse)
+            .Chunk(2)
+            .ToList();
+
+        var currentLocationRanges = new List<long[]>(seedRanges);
         var maps = inputSections[1..].Select(ParseMap);
 
-        var locationNumbers = new long[seeds.Length];
-        seeds.CopyTo(locationNumbers, 0);
-
-        for (var i = 0; i < seeds.Length; i++)
+        foreach (var map in maps)
         {
-            var currentLocation = locationNumbers[i];
-            foreach (var map in maps)
+            var newLocationRanges = new List<long[]>();
+            foreach (var locationRange in currentLocationRanges)
             {
-                var matchingMapping = map.FirstOrDefault(mapping =>
+                try
                 {
-                    var destination = mapping[0];
-                    var source = mapping[1];
-                    var range = mapping[2];
-                    return source <= currentLocation && currentLocation < source + range;
-                }, new long[] { 0, 0, 0 });
-
-                var destination = matchingMapping[0];
-                var source = matchingMapping[1];
-                currentLocation = destination + currentLocation - source;
+                    newLocationRanges.AddRange(MapToNewLocations(locationRange[0], locationRange[1], map));
+                } catch (StackOverflowException ex)
+                {
+                    Console.WriteLine(ex.StackTrace);
+                }
             }
-            locationNumbers[i] = currentLocation;
+            currentLocationRanges = newLocationRanges;
+        }
+        return currentLocationRanges.Min(x => x[0]).ToString();
+    }
+
+    public static List<long[]> MapToNewLocations(long location, long length, long[][] map)
+    {
+        var newLocations = new List<long[]>();
+        var matchingMapping = map.FirstOrDefault(mapping =>
+        {
+            var destination = mapping[0];
+            var source = mapping[1];
+            var range = mapping[2];
+            if (source + range < location)
+                return false;
+            if (location + length < source)
+                return false;
+            return true;
+
+            //return location <= source && source < (location + length)
+            //|| (location + length) >= (source + range) && location < (source + range) && (source + range) <= (location + length);
+        });
+
+        if (matchingMapping is null)
+        {
+            newLocations.Add(new long[] { location, length });
+            return newLocations;
         }
 
-        return locationNumbers.Min().ToString();
+        var destination = matchingMapping[0];
+        var source = matchingMapping[1];
+        var range = matchingMapping[2];
+
+        var leftLength = source - location;
+        if (leftLength > 0)
+        {
+            newLocations.AddRange(MapToNewLocations(location, leftLength, map));
+        }
+
+        var rightLength = location + length - source - range;
+        if (rightLength > 0)
+        {
+            newLocations.AddRange(MapToNewLocations(location + length - rightLength - 1, rightLength, map));
+        }
+        var midLength = length - Math.Max(leftLength, 0) - Math.Max(rightLength, 0);
+        newLocations.Add(new long[] { destination, midLength });
+        return newLocations;
     }
 }
