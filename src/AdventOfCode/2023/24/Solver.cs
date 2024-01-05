da0cbaee5ff22eff83395dc5e50c413bfb442885
Eskil Høstad <eskil.hostad@huddly.com>
2024-01-05 21:08:23 +0100

    cheat my way through 2023 day 24

--- src/AdventOfCode/2023/24/Solver.cs
+++ src/AdventOfCode/2023/24/Solver.cs
@@ -1,8 +1,4 @@
-using AdventOfCode.Common;
-using AdventOfCode.Y2022.D12;
 using Extreme.Mathematics;
-using MathNet.Numerics.LinearAlgebra.Complex;
-using Microsoft.VisualBasic;
 
 namespace AdventOfCode.Y2023.D24;
 
@@ -24,13 +20,14 @@ internal class Solver : ISolver
                         return new Vector2(nums[0], nums[1]);
                     })
                     .ToArray();
-                return (p: vectors[0], v: vectors[1]);
+                return new Hail(Position: vectors[0], Velocity: vectors[1]);
             })
             .ToArray();
 
-        BigFloat min = 2e14;
-        BigFloat max = 4e14;
-        bool IsWithinBounds(Vector2 vector) => min <= vector.X && vector.X <= max && min <= vector.Y && vector.Y <= max;
+        decimal min = 200_000_000_000_000;
+        decimal max = 400_000_000_000_000;
+        bool IsWithinBounds(Vector2 vector) =>
+            min <= vector.X0 && vector.X0 <= max && min <= vector.X1 && vector.X1 <= max;
 
         var futureIntersections = hails[0..^1].SelectMany(
             (hail, i) =>
@@ -43,23 +40,24 @@ internal class Solver : ISolver
         return futureIntersections.Count();
     }
 
-    static Vector2? FutureIntersection((Vector2 p, Vector2 v) first, (Vector2 p, Vector2 v) second)
+    static Vector2? FutureIntersection(Hail first, Hail second)
     {
-        var vCrossProduct = CrossProduct(first.v, second.v);
-        var areParallel = vCrossProduct == BigFloat.Zero;
+        var vCrossProduct = CrossProduct(first.Velocity, second.Velocity);
+        var areParallel = vCrossProduct == 0;
         if (areParallel)
             return null;
 
-        var firstT = CrossProduct(second.p - first.p, second.v) / vCrossProduct;
-        var secondT = CrossProduct(first.p - second.p, first.v) / (vCrossProduct * (-1));
-        if (firstT < BigFloat.Zero || secondT < BigFloat.Zero)
+        var firstT = CrossProduct(second.Position - first.Position, second.Velocity) / vCrossProduct;
+        var secondT = CrossProduct(first.Position - second.Position, first.Velocity) / (vCrossProduct * (-1));
+        if (firstT < 0 || secondT < 0)
             return null;
 
-        return first.p + firstT * first.v;
+        return first.Position + firstT * first.Velocity;
     }
 
-    static BigFloat CrossProduct(Vector2 a, Vector2 b) => a.X * b.Y - b.X * a.Y;
+    static decimal CrossProduct(Vector2 a, Vector2 b) => a.X0 * b.X1 - b.X0 * a.X1;
 
+    /// Blatant ripoff of https://github.com/encse/adventofcode/tree/master/2023/Day24
     public object PartTwo(string input)
     {
         var hails = input
@@ -69,37 +67,92 @@ internal class Solver : ISolver
                 var vectors = line.Split('@', StringSplitOptions.TrimEntries)
                     .Select(section =>
                     {
-                        return section
+                        var nums = section
                             .Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
-                            .Select(decimal.Parse)
+                            .Select(long.Parse)
                             .ToArray();
+                        return (x: nums[0], y: nums[1], z: nums[2]);
                     })
                     .ToArray();
-
-                return (p: vectors[0], v: vectors[1]);
+                return (pos: vectors[0], vel: vectors[1]);
             })
-            // Only need three lines to setup the equation
-            .Take(3)
             .ToArray();
 
-        if (!(hails is [var A, var B, var C])) throw new InvalidOperationException();
-        return "Not available";
+        var hailsXY = hails
+            .Select(h => new Hail(new Vector2(h.pos.x, h.pos.y), new Vector2(h.vel.x, h.vel.y)))
+            .ToArray();
+        var hailsXZ = hails
+            .Select(h => new Hail(new Vector2(h.pos.x, h.pos.z), new Vector2(h.vel.x, h.vel.z)))
+            .ToArray();
+
+        var stoneXY = Solve2D(hailsXY);
+        var stoneXZ = Solve2D(hailsXZ);
+        return stoneXY.X0 + stoneXY.X1 + stoneXZ.X1;
     }
 
+    static Vector2 Solve2D(Hail[] hails)
+    {
+        // Try to guess the velocity of our stone (a for loop), then supposing
+        // that it is the right velocity we create a new reference frame that
+        // moves with that speed. The stone doesn't move in this frame, it has
+        // some fixed unknown coordinate. Now transform each particle into
+        // this reference frame as well. Since the stone is not moving, if we
+        // properly guessed the speed, we find that each particle meets at the
+        // same point. This must be the stone's location.
 
+        Hail TranslateVelocity(Hail hail, Vector2 vel) => new Hail(hail.Position, hail.Velocity - vel);
+
+        var s = 500; //arbitrary +- limit for the brute force
+        for (var v1 = -s; v1 < s; v1++)
+        {
+            for (var v2 = -s; v2 < s; v2++)
+            {
+                var vel = new Vector2(v1, v2);
+
+                var stone = Intersection(TranslateVelocity(hails[0], vel), TranslateVelocity(hails[1], vel));
+                if (!stone.HasValue)
+                    continue;
+
+                if (hails.All(h => IntersectsPoint(TranslateVelocity(h, vel), stone.Value)))
+                {
+                    return stone.Value;
+                }
+            }
+        }
+        throw new Exception("No suitable stone found");
+    }
+
+    static Vector2? Intersection(Hail first, Hail second)
+    {
+        var vCrossProduct = CrossProduct(first.Velocity, second.Velocity);
+        var areParallel = vCrossProduct == 0;
+        if (areParallel)
+            return null;
+
+        var firstT = CrossProduct(second.Position - first.Position, second.Velocity) / vCrossProduct;
+
+        return first.Position + firstT * first.Velocity;
+    }
+
+    static bool IntersectsPoint(Hail hail, Vector2 point)
+    {
+        var d = (point.X0 - hail.Position.X0) * hail.Velocity.X1 - (point.X1 - hail.Position.X1) * hail.Velocity.X0;
+        return Math.Abs(d) < (decimal)0.0001;
+    }
 }
 
-public readonly struct Vector2(BigFloat x, BigFloat y)
+public record struct Hail(Vector2 Position, Vector2 Velocity);
+
+public readonly struct Vector2(decimal x0, decimal x1)
 {
-    public readonly BigFloat X = x;
-    public readonly BigFloat Y = y;
+    public readonly decimal X0 = x0;
+    public readonly decimal X1 = x1;
 
-    public static Vector2 operator +(Vector2 left, Vector2 right) => new(left.X + right.X, left.Y + right.Y);
+    public static Vector2 operator +(Vector2 left, Vector2 right) => new(left.X0 + right.X0, left.X1 + right.X1);
 
-    public static Vector2 operator -(Vector2 left, Vector2 right) => new(left.X - right.X, left.Y - right.Y);
+    public static Vector2 operator -(Vector2 left, Vector2 right) => new(left.X0 - right.X0, left.X1 - right.X1);
 
-    public static Vector2 operator *(BigFloat scalar, Vector2 vector) => vector * scalar;
+    public static Vector2 operator *(decimal scalar, Vector2 vector) => vector * scalar;
 
-    public static Vector2 operator *(Vector2 vector, BigFloat scalar) =>
-        new(vector.X * scalar, vector.Y * scalar);
+    public static Vector2 operator *(Vector2 vector, decimal scalar) => new(vector.X0 * scalar, vector.X1 * scalar);
 }
