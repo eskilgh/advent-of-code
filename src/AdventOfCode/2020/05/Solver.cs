702f2cf27cdacc2d4b31c8e11111385af6f85a23
Eskil Høstad <eskil.hostad@huddly.com>
2024-01-07 20:23:22 +0100

    simplify

--- src/AdventOfCode/2020/05/Solver.cs
+++ src/AdventOfCode/2020/05/Solver.cs
@@ -1,38 +1,28 @@
-using AdventOfCode.Common;
-
 namespace AdventOfCode.Y2020.D05;
-
-record Seat(int Row, int Column)
-{
-    public int Id => Row * 8 + Column;
-}
 
 internal class Solver : ISolver
 {
     public object PartOne(string input)
     {
-        var seats = ParseSeats(input);
-        return seats.Max(seat => seat.Id);
+        var seats = ParseSeatIds(input);
+        return seats.Max();
     }
 
     public object PartTwo(string input)
     {
-        var seats = ParseSeats(input).ToList();
-        seats.Sort((a, b) => a.Id - b.Id);
-        var seatIdStart = seats.First().Id;
-        var missingSeat = seats.Where((seat, i) => seat.Id != (i + seatIdStart)).First().Id - 1;
-        return missingSeat;
+        var seatIds = ParseSeatIds(input);
+        var allSeatIds = Enumerable.Range(seatIds.Min(), seatIds.Count()).ToHashSet();
+        return allSeatIds.Except(seatIds).Single();
     }
 
-    private static IEnumerable<Seat> ParseSeats(string input)
+    private static IEnumerable<int> ParseSeatIds(string input)
     {
         return input
+            .Replace('B', '1')
+            .Replace('F', '0')
+            .Replace('R', '1')
+            .Replace('L', '0')
             .Split('\n')
-            .Select(line =>
-            {
-                var row = Convert.ToInt32(line[0..7].Replace('B', '1').Replace('F', '0'), 2);
-                var column = Convert.ToInt32(line[7..].Replace('R', '1').Replace('L', '0'), 2);
-                return new Seat(row, column);
-            });
+            .Select(binaryNumber => Convert.ToInt32(binaryNumber, 2));
     }
 }
