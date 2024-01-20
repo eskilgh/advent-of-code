bde71210ab6645a1d0c6e15b91079e97ddedd58f
Eskil Høstad <eskil.hostad@huddly.com>
2024-01-20 18:07:00 +0100

    2020 day 16

--- src/AdventOfCode/2020/16/Solver.cs
+++ src/AdventOfCode/2020/16/Solver.cs
@@ -3,23 +3,51 @@ using AdventOfCode.Common;
 
 namespace AdventOfCode.Y2020.D16;
 
-record Rule(string Field, Interval Interval);
-
-record struct Ticket(int X1, int X2, int X3);
-
 internal class Solver : ISolver
 {
     public object PartOne(string input)
     {
         var (rules, _, nearbyTickets) = Parse(input);
 
-        bool IsValidValue(int value) => rules.Values.SelectMany(ranges => ranges).Any(range => range.Contains(value));
-        return nearbyTickets.SelectMany(ticket => ticket.Where(IsValidValue)).Sum();
+        return nearbyTickets.SelectMany(ticket => ticket.Where(value => IsValidValue(rules, value))).Sum();
     }
 
-    static (Dictionary<string, IEnumerable<Interval>> rules, int[] ticket, IEnumerable<int[]> nearbyTickets) Parse(
-        string input
-    )
+    public object PartTwo(string input)
+    {
+        var (rules, myTicket, nearbyTickets) = Parse(input);
+        var validTickets = nearbyTickets.Where(ticket => ticket.All(value => IsValidValue(rules, value)));
+
+        var valuesByPosition = validTickets.Aggregate(
+            myTicket.Select((_) => Enumerable.Empty<int>()),
+            (acc, ticket) => acc.Select((vs, i) => vs.Append(ticket[i]))
+        );
+
+        var validRulesForPositions = valuesByPosition.Select(
+            vs => rules.Where(kvp => vs.All(v => kvp.Value.Any(range => range.Contains(v)))).ToHashSet()
+        );
+
+        // For my input this was sufficient for ending up with one rule per position
+        // Other input may require more advanced 'sudoku'-solving
+        while (validRulesForPositions.Any(rules => rules.Count > 1))
+        {
+            var takenRules = validRulesForPositions.Where(rules => rules.Count == 1).SelectMany(r => r).ToHashSet();
+            validRulesForPositions = validRulesForPositions.Select(
+                rules => rules.Count == 1 ? rules : rules.Except(takenRules).ToHashSet()
+            );
+        }
+
+        return validRulesForPositions
+            .Select((rules, i) => (ruleName: rules.Single().Key, i))
+            .Where(tpl => tpl.ruleName.StartsWith("departure"))
+            .Aggregate((long)1, (acc, tpl) => acc * myTicket[tpl.i]);
+    }
+
+    static bool IsValidValue(Dictionary<string, Interval[]> rules, int value)
+    {
+        return rules.Values.SelectMany(ranges => ranges).Any(range => range.Contains(value));
+    }
+
+    static (Dictionary<string, Interval[]> rules, int[] ticket, IEnumerable<int[]> nearbyTickets) Parse(string input)
     {
         var sections = input.Split("\n\n");
 
@@ -37,15 +65,11 @@ internal class Solver : ISolver
                     {
                         var nums = m.Value.Split('-').Select(int.Parse);
                         return new Interval(nums.First(), nums.Last());
-                    });
+                    })
+                    .ToArray();
                 return (field, ranges);
             })
             .ToDictionary(tpl => tpl.field, tpl => tpl.ranges);
         return (rules, ticket, nearbyTickets);
-    }
-
-    public object PartTwo(string input)
-    {
-        return "Not available";
     }
 }
