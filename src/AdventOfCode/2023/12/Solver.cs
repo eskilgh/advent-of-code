f9b9d9309a3fa5991b5013177ad819f28374779c
Eskil Høstad <eskilgh@gmail.com>
2024-03-02 09:55:00 +0100

    cleanup

--- src/AdventOfCode/2023/12/Solver.cs
+++ src/AdventOfCode/2023/12/Solver.cs
@@ -6,24 +6,10 @@ internal class Solver : ISolver
 {
     public object PartOne(string input)
     {
-        return "susepended";
-        var lines = input.Split('\n');
-        var conditions = lines
-            .Select(line => line.Split(" ", StringSplitOptions.RemoveEmptyEntries)[0].ToCharArray())
-            .ToArray();
-        var groupings = lines
-            .Select(
-                line => line.Split(" ", StringSplitOptions.RemoveEmptyEntries)[1].Split(',').Select(int.Parse).ToArray()
-            )
-            .ToArray();
+        return Parse(input).Select(tpl => FindPermutations(tpl.conditions, tpl.groupings)).Sum();
 
-        var permutations = conditions.Zip(groupings, FindPermutationsMemoized).ToArray();
-        //Console.WriteLine(string.Join(" ", permutations));
-        return permutations.Sum();
-        //var permutationsPerConditions = conditions.Zip(groupings, FindPerms).ToArray();
-
-        //Console.WriteLine(FindPermutations(conditions[0], groupings[0]));
-        /*for (var i = 0; i < permutationsPerConditions.Length; i++)
+        /*
+        for (var i = 0; i < permutationsPerConditions.Length; i++)
         {
             var condition = conditions[i];
             Console.WriteLine("\n" + new string(condition));
@@ -39,36 +25,27 @@ internal class Solver : ISolver
             }
         }
         return permutationsPerConditions.Select(ps => ps.Count()).Sum();
-*/
+        */
     }
 
-    static List<List<(int pos, int groupSize)>> FindPerms(char[] cs, int[] groups)
+    public object PartTwo(string input)
     {
-        return FindPermsRec(cs, groups, 0, 0);
+        var data = Parse(input).ToArray();
+
+        //var permutations = unfoldedConditions.Zip(unfoldedGroupings, FindPermutationsMemoized).ToArray();
+        return "not available";
     }
 
-    static List<List<(int start, int groupSize)>> FindPermsRec(char[] cs, int[] groups, int startPos, int groupIdx)
+    static IEnumerable<(char[] conditions, int[] groupings)> Parse(string input)
     {
-        if (groupIdx >= groups.Length)
-            return new List<List<(int, int)>>();
 
-        var groupSize = groups[groupIdx];
-        var possiblePermutations = new List<List<(int, int)>>();
-        var pos = startPos;
-        var canPlaceGroupAtCurrentPos =
-            (pos + groupSize) <= cs.Length
-            && cs[pos..(pos + groupSize)].All(c => c is '?' or '#')
-            && (pos + groupSize >= cs.Length || cs[pos + groupSize] is '?' or '.');
-        if (canPlaceGroupAtCurrentPos)
+        return input.Split('\n').Select(line =>
         {
-            FindPermsRec(cs, groups, pos + groupSize + 1, groupIdx + 1)
-                .Select(e =>
-                {
-                    List<(int, int)> placements = [.. e, (pos, groupSize)];
-                    return placements;
-                });
-        }
-        return possiblePermutations;
+            var sections = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var conditions = sections[0].ToCharArray();
+            var groupings = sections[1].Split(',').Select(int.Parse).ToArray();
+            return (conditions, groupings);
+        });
     }
 
     public static int FindUnfoldedPermutations(char[] cs, int[] groups)
@@ -121,101 +98,6 @@ internal class Solver : ISolver
             pos++;
         }
         return possiblePermutations;
-    }
-
-    public static long FindPermutationsMemoized(char[] cs, int[] groups)
-    {
-        var memo = new Dictionary<(char[] cs, int[] groups, int i, int groupIdx), int>();
-        return FindPermutationsRecMemoized(cs, groups, 0, 0, memo);
-    }
-
-    static int FindPermutationsRecMemoized(char[] cs, int[] groups, int i, int groupIdx, IDictionary<(char[] cs, int[] groups, int i, int groupIdx), int> memo)
-    {
-        var input = (cs, groups, i, groupIdx);
-        if (memo.TryGetValue(input, out var memoed))
-            return memoed;
-        if (groupIdx >= groups.Length)
-        {
-            // Not valid if remaining '#'s 
-            var ans = i >= cs.Length || cs[i..].All(c => c is not '#') ? 1 : 0;
-            memo[input] = ans;
-            return ans;
-        }
-        if (i >= cs.Length)
-        {
-            var ans = 0;
-            memo[input] = ans;
-            return ans;
-        }
-
-        var groupSize = groups[groupIdx];
-        var possiblePermutations = 0;
-        var pos = i;
-        var nextBrokenSpring = Array.IndexOf(cs, '#', i);
-        while ((pos + groupSize) <= cs.Length && (nextBrokenSpring == -1 || pos <= nextBrokenSpring))
-        {
-            var canPlaceGroupAtCurrentPos =
-                cs[pos..(pos + groupSize)].All(c => c is '?' or '#')
-                // if not at the end, must have a '.' between '#'
-                && (pos + groupSize >= cs.Length || cs[pos + groupSize] is '?' or '.');
-            if (canPlaceGroupAtCurrentPos)
-            {
-                possiblePermutations += FindPermutationsRec(cs, groups, pos + groupSize + 1, groupIdx + 1);
-                var isTheOnlyValidPlacementLeft = cs[pos..(pos + groupSize)].All(c => c is '#');
-                if (isTheOnlyValidPlacementLeft)
-                {
-                    memo[input] = possiblePermutations;
-                    return possiblePermutations;
-                }
-            }
-        
-            pos++;
-        }
-        memo[input] = possiblePermutations;
-        return possiblePermutations;
-    }
-
-    public object PartTwo(string input)
-    {
-        var lines = input.Split('\n');
-        var conditions = lines
-            .Select(line => line.Split(" ", StringSplitOptions.RemoveEmptyEntries)[0].ToCharArray())
-            .ToArray();
-        var groupings = lines
-            .Select(
-                line => line.Split(" ", StringSplitOptions.RemoveEmptyEntries)[1].Split(',').Select(int.Parse).ToArray()
-            )
-            .ToArray();
-
-        var unfoldedConditions = conditions.Select(cs =>
-        {
-            var ss = string.Join("?", Enumerable.Repeat(new string(cs), 5));
-            return ss.ToCharArray();
-        }).ToArray();
-        var unfoldedGroupings = groupings.Select(groups => Enumerable.Repeat(groups, 5).SelectMany(x => x).ToArray());
-
-        var totalItems = unfoldedConditions.Length;
-        var doneItems = 0;
-        var permutations = unfoldedConditions
-            .Zip(unfoldedGroupings, (condition, grouping) => new { Condition = condition, Grouping = grouping })
-            .AsParallel()
-            .Select((item, i) =>
-            {
-                var perms = FindPermutationsMemoized(item.Condition, item.Grouping);
-                UpdateProgressBar((++doneItems) * 100 / totalItems);
-                return perms;
-            }).ToArray();
-
-        Console.WriteLine();
-        //var permutations = unfoldedConditions.Zip(unfoldedGroupings, FindPermutationsMemoized).ToArray();
-        return permutations.Sum();
-    }
-
-    // Add a method to update the progress bar
-    void UpdateProgressBar(int percentage)
-    {
-        //Console.Clear();
-        Console.WriteLine($"Progress: {percentage}%");
     }
 
     
