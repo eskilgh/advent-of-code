527fbeb1169816ecbcebcd6fadc621a0e1400554
Eskil Høstad <eskilgh@gmail.com>
2024-03-03 21:12:31 +0100

    2023 day 12

--- src/AdventOfCode/2023/12/Solver.cs
+++ src/AdventOfCode/2023/12/Solver.cs
@@ -1,132 +1,54 @@
-using AngleSharp.Html;
-
 namespace AdventOfCode.Y2023.D12;
 
 internal class Solver : ISolver
 {
     public object PartOne(string input)
     {
-        return Parse(input).Select(tpl => FindPermutations1(new string(tpl.conditions), tpl.groupings)).Sum();
-
-        /*
-        for (var i = 0; i < permutationsPerConditions.Length; i++)
-        {
-            var condition = conditions[i];
-            Console.WriteLine("\n" + new string(condition));
-            var permutations = permutationsPerConditions[i];
-            foreach (var permutation in permutations)
-            {
-                foreach (var (pos, groupSize) in permutation)
-                {
-                    for (var offset = 0; offset < groupSize; offset++)
-                        condition[pos + offset] = '#';
-                }
-                Console.WriteLine(new string(condition).Replace('?', '.'));
-            }
-        }
-        return permutationsPerConditions.Select(ps => ps.Count()).Sum();
-        */
+        return Parse(input)
+            .Select(tpl => FindPermutations1(tpl.record, tpl.groups))
+            .Sum();
     }
 
     public object PartTwo(string input)
     {
         var answers = Parse(input)
-            .Select(tpl => Unfold(tpl.conditions, tpl.groupings))
-            .Select((tpl) => FindPermutations1(new string(tpl.conditions), tpl.groups)).ToArray();
+            .Select(tpl => Unfold(tpl.record, tpl.groups))
+            .Select(tpl => FindPermutations1(tpl.record, tpl.groups)).ToArray();
         return answers.Sum();
-        Console.WriteLine("------");
-        var parsed = Parse(input).ToArray();
-        var i = 0;
-        foreach (var num in answers)
-        {
-            Console.Write(new string(parsed[i].conditions) + " [" + string.Join(" ", parsed[i].groupings) + "] ");
-            Console.WriteLine(num);
-            i++;
-        }
-
-        /*
-        for (var i = 0; i < permutationsPerConditions.Length; i++)
-        {
-            var condition = conditions[i];
-            Console.WriteLine("\n" + new string(condition));
-            var permutations = permutationsPerConditions[i];
-            foreach (var permutation in permutations)
-            {
-                foreach (var (pos, groupSize) in permutation)
-                {
-                    for (var offset = 0; offset < groupSize; offset++)
-                        condition[pos + offset] = '#';
-                }
-                Console.WriteLine(new string(condition).Replace('?', '.'));
-            }
-        }
-        return permutationsPerConditions.Select(ps => ps.Count()).Sum();
-        */
-        return answers.Sum();
-        //var permutations = unfoldedConditions.Zip(unfoldedGroupings, FindPermutationsMemoized).ToArray();
-        return "not available";
     }
 
-    static IEnumerable<(char[] conditions, int[] groupings)> Parse(string input)
+    static IEnumerable<(string record, int[] groups)> Parse(string input)
     {
         return input.Split('\n').Select(line =>
         {
             var sections = line.Split(" ",
                 StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.RemoveEmptyEntries);
-            var conditions = sections[0].ToCharArray();
-            var groupings = sections[1].Split(',').Select(int.Parse).ToArray();
-            return (conditions, groupings);
+            var groups = sections[1].Split(',').Select(int.Parse).ToArray();
+            return (sections[0], groups);
         });
     }
 
-    public static long FindUnfoldedPermutations(char[] cs, int[] groups)
+    public static (string record, int[] groups) Unfold(string record, int[] groups)
     {
-        var basePermutations = FindPermutations(cs, groups);
-        var prefPerms = FindPermutations(['?', ..cs], groups);
-        var sufPerms = FindPermutations([..cs, '?'], groups);
-        var subPermutations = FindPermutations(['?', .. cs], groups);
-        if (subPermutations == basePermutations)
-        {
-            var suffixedPermutations = FindPermutations([.. cs, '?'], groups);
-            return basePermutations * Convert.ToInt64(Math.Pow(suffixedPermutations, 4));
-        }
-
-        char[] csEndingWithBroken = [..cs[..^1], '#'];
-        var basePermutationsEndingWithBroken = FindPermutations(csEndingWithBroken, groups);
-        var subPermutationsBeginningWithOperational = FindPermutations(['.', ..cs[1..]], groups);
-        var numPermutationsForBaseEndingWithBroken = cs switch
-        {
-            [.. , '.'] => 0,
-            _ => Convert.ToInt64(Math.Pow(basePermutationsEndingWithBroken, 4)),
-        };
-
-
-        char[] csEndingWithOperational = [..cs[..^1], '.'];
-        var basePermutationsEndingWithOperational = FindPermutations(csEndingWithOperational, groups);
-        var numPermutationsForBaseEndingWithOperational =
-            cs switch
-            {
-                [.. , '#'] => 0,
-                _ => basePermutationsEndingWithOperational * Convert.ToInt64(Math.Pow(subPermutations, 4))
-            };
-
-        return numPermutationsForBaseEndingWithBroken + numPermutationsForBaseEndingWithOperational;
-
-        //var subPermutationsBeginningWithBroken = FindPermutations(['#', ..cs[1..]], groups);
-        //return basePermutations * Convert.ToInt32(Math.Pow(subPermutations, 4));
+        var unfoldedRecord = string.Join('?', Enumerable.Repeat(record, 5));
+        var unfoldedGroups = Enumerable.Repeat(groups, 5).SelectMany(x => x).ToArray();
+        return (unfoldedRecord, unfoldedGroups);
     }
 
-    private static Dictionary<(string, int[]), long> memo = new();
+
     public static long FindPermutations1(string record, int[] groups)
     {
-        //var memo = new Dictionary<(string, int[]), long>();
+        var memo = new Dictionary<(string, string), long>();
         return FindPermutations1Rec(record, groups, memo);
     }
-    static long FindPermutations1Rec(string record, int[] groups, Dictionary<(string, int[]), long> memo)
+
+    static long FindPermutations1Rec(string record, int[] groups, Dictionary<(string, string), long> memo)
     {
-        if (!memo.ContainsKey((record, groups)))
+        // Use a string for comparing groups to get 'value' equality
+        var key = (record, string.Join(',', groups));
+        if (!memo.ContainsKey(key))
         {
-            memo[(record, groups)] = record.FirstOrDefault() switch
+            memo[key] = record.FirstOrDefault() switch
             {
                 '.' => PermutationsForOperational(record, groups, memo),
                 '#' => PermutationsForBroken(record, groups, memo),
@@ -134,22 +56,22 @@ internal class Solver : ISolver
                 _ when groups.Length is 0 => 1,
                 _ => 0,
             };
-
         }
-        return memo[(record, groups)];
+
+        return memo[key];
     }
 
-    static long PermutationsForWildcard(string record, int[] groups, Dictionary<(string, int[]), long> memo)
+    static long PermutationsForWildcard(string record, int[] groups, Dictionary<(string, string), long> memo)
     {
         return PermutationsForOperational(record, groups, memo) + PermutationsForBroken(record, groups, memo);
     }
 
-    static long PermutationsForOperational(string record, int[] groups, Dictionary<(string, int[]), long> memo)
+    static long PermutationsForOperational(string record, int[] groups, Dictionary<(string, string), long> memo)
     {
         return FindPermutations1Rec(record[1..], groups, memo);
     }
 
-    static long PermutationsForBroken(string record, int[] groups, Dictionary<(string, int[]), long> memo)
+    static long PermutationsForBroken(string record, int[] groups, Dictionary<(string, string), long> memo)
     {
         if (groups.Length is 0)
             // '#' left after filling all groups -> not valid
@@ -171,50 +93,5 @@ internal class Solver : ISolver
             // Make sure a '.' follows the group
             _ => FindPermutations1Rec(record[(targetSize + 1)..], groups[1..], memo)
         };
-    }
-
-
-    public static int FindPermutations(char[] cs, int[] groups)
-    {
-        return FindPermutationsRec(cs, groups, 0, 0);
-    }
-
-    static int FindPermutationsRec(char[] cs, int[] groups, int i, int groupIdx)
-    {
-        if (groupIdx >= groups.Length)
-            // Not valid if remaining '#'s 
-            return i >= cs.Length || cs[i..].All(c => c is not '#') ? 1 : 0;
-        if (i >= cs.Length)
-            return 0;
-
-        var groupSize = groups[groupIdx];
-        var possiblePermutations = 0;
-        var pos = i;
-        var nextBrokenSpring = Array.IndexOf(cs, '#', i);
-        while ((pos + groupSize) <= cs.Length && (nextBrokenSpring == -1 || pos <= nextBrokenSpring))
-        {
-            var canPlaceGroupAtCurrentPos =
-                cs[pos..(pos + groupSize)].All(c => c is '?' or '#')
-                // if not at the end, must have a '.' between '#'
-                && (pos + groupSize >= cs.Length || cs[pos + groupSize] is '?' or '.');
-            if (canPlaceGroupAtCurrentPos)
-            {
-                possiblePermutations += FindPermutationsRec(cs, groups, pos + groupSize + 1, groupIdx + 1);
-                var isTheOnlyValidPlacementLeft = cs[pos..(pos + groupSize)].All(c => c is '#');
-                if (isTheOnlyValidPlacementLeft)
-                    return possiblePermutations;
-            }
-
-            pos++;
-        }
-
-        return possiblePermutations;
-    }
-
-    public static (char[] conditions, int[] groups) Unfold(char[] conditions, int[] groups)
-    {
-        var newConditions = string.Join('?', Enumerable.Range(0, 5).Select(_ => new string(conditions))).ToCharArray();
-        var newGroups = Enumerable.Range(0, 5).SelectMany(_ => groups).ToArray();
-        return (newConditions, newGroups);
     }
 }
--- test/UnitTests/Tests.cs
+++ test/UnitTests/Tests.cs
@@ -5,70 +5,12 @@ using Day12Solver = AdventOfCode.Y2023.D12.Solver;
 
 namespace UnitTests;
 
-public class Tests
+public class Tests(ITestOutputHelper testOutputHelper)
 {
-    private readonly ITestOutputHelper _testOutputHelper;
-
-    public Tests(ITestOutputHelper testOutputHelper)
-    {
-        _testOutputHelper = testOutputHelper;
-    }
 
     [Fact]
-    public void Test1()
+    public void DummyTest()
     {
-        var line = "???.###".ToCharArray();
-        var ans = Day12Solver.FindPermutations(line, [1,3]);
-        ans.Should().Be(3);
-    }
-
-    [Theory]
-    [InlineData("???.###", new int[] {1,1,3}, 1)]
-    [InlineData(".??..??...?##. 1,1,3", new int[] {1,1,3}, 4)]
-    [InlineData("?#?#?#?#?#?#?#?", new int[] {1,3,1,6}, 1)]
-    [InlineData("????.#...#...", new int[] {4,1,1}, 1)]
-    [InlineData("????.######..#####.", new int[] {1,6,5}, 4)]
-    [InlineData("?###????????", new int[] { 3, 2, 1 }, 10)]
-    [InlineData("?#???", new int[] { 2 }, 2)]
-    [InlineData("????#", new int[] { 1 }, 1)]
-    public void Test2(string line, int[] groups, int expected)
-    {
-        //var line = "?###????????".ToCharArray();
-        var ans = Day12Solver.FindPermutations1(line, groups);
-        ans.Should().Be(expected);
-    }
-
-    
-    [Theory]
-    [InlineData("???.###", new int[] {1,1,3}, 1)]
-    [InlineData(".??..??...?##.", new int[] {1,1,3}, 16384)]
-    [InlineData("?#?#?#?#?#?#?#?", new int[] {1,3,1,6}, 1)]
-    [InlineData("????.#...#...", new int[] {4,1,1}, 16)]
-    [InlineData("????.######..#####.", new int[] {1,6,5}, 2500)]
-    [InlineData("?###????????", new int[] { 3, 2, 1 }, 506250)]
-    [InlineData("?.????..??", new int[] { 1, 3, 1}, 5184)]
-    //[InlineData("?#???", new int[] { 2 }, 2)]
-    //[InlineData("????#", new int[] { 1 }, 1)]
-    public void TestFindUnfoldedPermutations(string line, int[] groups, int expected)
-    {
-        //var line = "?###????????".ToCharArray();
-        var ans = Day12Solver.FindUnfoldedPermutations(line.ToCharArray(), groups);
-        ans.Should().Be(expected);
-    }
-
-    [Fact]
-    public void TestFindAnswers()
-    {
-        var line = "?.????..??";
-
-        var d = string.Join('?', Enumerable.Range(0, 5).Select(_ => line));
-        int[] groups = [1, 3, 1];
-        var (newConditions, newGroups) = Day12Solver.Unfold(line.ToCharArray(), groups);
-        newConditions.Length.Should().Be(line.Length * 5 + 4);
-        newGroups.Length.Should().Be(groups.Length * 5);
-        var ans = Day12Solver.FindPermutations1(new string(newConditions), newGroups);
-        _testOutputHelper.WriteLine($"Answer: {ans}");
         true.Should().BeTrue();
     }
-    
 }
