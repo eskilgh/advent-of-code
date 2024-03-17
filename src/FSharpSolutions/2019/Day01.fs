a6d94685b172828730c421dc8d8dfea04bbca71a
Eskil Høstad <eskilgh@gmail.com>
2024-03-17 08:45:20 +0100

    2019 day 1

---
+++ src/FSharpSolutions/2019/Day01.fs
@@ -0,0 +1,25 @@
+ namespace FSharpSolutions.Y2019.D01
+ 
+ type Solver() =
+     interface Shared.ISolver with
+         member this.PartOne(input: string): obj =
+             input.Split('\n') 
+                |> Array.map int 
+                |> Array.map (fun m -> m / 3 - 2)
+                |> Array.sum 
+                |> box
+         member this.PartTwo(input: string): obj =
+            let rec calculateFuel m = 
+                match (m / 3 - 2) with
+                | m' when m' <= 0 -> 0
+                | m' -> (m' + calculateFuel m')
+            input.Split('\n')
+                |> Array.map int
+                |> Array.map calculateFuel
+                |> Array.sum
+                |> box
+
+
+
+
+ 
