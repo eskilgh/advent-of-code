b09207a77d4b9fdb4de92a90673694d3cdcf8601
Eskil Høstad <eskilgh@gmail.com>
2024-03-24 18:05:45 +0100

    go crazy with some active pattern refactoring

--- src/FSharpSolutions/2019/Day05.fs
+++ src/FSharpSolutions/2019/Day05.fs
@@ -1,6 +1,60 @@
 namespace FSharpSolutions.Y2019.D05
 
 module IntCode =
+    let (|Position|_|) value =
+        if value = 0 then
+            Some Position
+        else
+            None
+
+    let (|Immediate|_|) value =
+        if value = 1 then
+            Some Immediate
+        else
+            None
+
+
+    let (|Add|_|) value =
+        if value = 1 then
+            Some(fun a b -> a + b)
+        else
+            None
+
+    let (|Multiply|_|) value =
+        if value = 2 then
+            Some(fun a b -> a * b)
+        else
+            None
+
+    let (|Input|_|) value = if value = 3 then Some Input else None
+    let (|Output|_|) value = if value = 4 then Some Output else None
+
+    let (|JumpIfTrue|_|) value =
+        if value = 5 then
+            Some JumpIfTrue
+        else
+            None
+
+    let (|JumpIfFalse|_|) value =
+        if value = 6 then
+            Some JumpIfFalse
+        else
+            None
+
+    let (|LessThan|_|) value =
+        if value = 7 then
+            Some(fun a b -> a < b)
+        else
+            None
+
+    let (|Equals|_|) value =
+        if value = 8 then
+            Some(fun a b -> a = b)
+        else
+            None
+
+    let (|Halt|_|) value = if value = 99 then Some Halt else None
+
     let execute (opcodes: int []) (input: int) =
         let rec executeRec i =
             seq {
@@ -8,43 +62,42 @@ module IntCode =
                     let mode = opcodes.[i] / (pown 10 (offset + 1)) % 10
 
                     match mode with
-                    | 0 -> opcodes.[opcodes.[i + offset]]
-                    | 1 -> opcodes.[i + offset]
+                    | Position -> opcodes.[opcodes.[i + offset]]
+                    | Immediate -> opcodes.[i + offset]
                     | _ -> failwith "Unexpected parameter mode"
 
                 let opcode = opcodes.[i] % 100
 
                 match opcode with
-                | 1
-                | 2 ->
-                    let op = if opcode = 1 then (+) else (*)
+                | Add op
+                | Multiply op ->
                     let a, b, address = (getParam 1, getParam 2, opcodes.[i + 3])
                     opcodes.[address] <- op a b
                     yield! executeRec (i + 4)
-                | 3 ->
+                | Input ->
                     let address = opcodes[i + 1]
                     opcodes.[address] <- input
                     yield! executeRec (i + 2)
-                | 4 ->
+                | Output ->
                     let a = getParam 1
                     yield a
                     yield! executeRec (i + 2)
-                | 5
-                | 6 ->
+                | JumpIfTrue
+                | JumpIfFalse ->
                     let a, b = getParam 1, getParam 2
 
                     match opcode with
                     | 5 when a <> 0 -> yield! executeRec b
                     | 6 when a = 0 -> yield! executeRec b
                     | _ -> yield! executeRec (i + 3)
-                | 7
-                | 8 ->
+
+                | LessThan comparator
+                | Equals comparator ->
                     let a, b, address = getParam 1, getParam 2, opcodes[i + 3]
-                    let comparator = if opcode = 7 then (<) else (=)
                     let value = if comparator a b then 1 else 0
                     opcodes.[address] <- value
                     yield! executeRec (i + 4)
-                | 99 -> yield! []
+                | Halt -> yield! []
                 | n -> failwith $"Invalid opcode: {n}"
             }
 
